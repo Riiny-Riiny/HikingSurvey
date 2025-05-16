a05ec92e653be307ac68c930124b0c3bd60c3e73
Riiny Giir <rgiir2020@riordanhs.org>
2025-05-15 21:51:52 -0400

    Initial Commit

---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Riiny Giir on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
